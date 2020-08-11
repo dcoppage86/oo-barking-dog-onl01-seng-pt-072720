bac96ae3109ba588af72d37e398bd26fe1e19edb
Dakota Coppage <dakotakcoppage@me.com>
2020-08-11 22:57:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog 
+  
+  def name=(dogs_name)
+    @fido = dogs_name
+  end
+  
+  def name
+    @fido
+  end
+end
+
+class Dog
+  def bark
+    puts "woof!"
+  end
+end
+  
+
+
